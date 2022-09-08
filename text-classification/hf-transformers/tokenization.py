7f51839ffd76a0884566184a5c9c1e009bf84a81
LakshyaMalhotra <lakshya9009@gmail.com>
2022-09-08 07:23:41 -0700

    Add function for tokenizing the whole dataset.

--- text-classification/hf-transformers/tokenization.py
+++ text-classification/hf-transformers/tokenization.py
@@ -1,21 +1,8 @@
-import pandas as pd
-from datasets import Dataset, load_dataset
+import datasets as hfd
 from transformers import AutoTokenizer
 
 
-if __name__ == "__main__":
-    # load dataset
-    emotions = load_dataset("emotion")
-
-    print(emotions)
-    train_ds = emotions["train"]
-    print(train_ds.column_names)
-    print(train_ds[15:17])
-
-    # load model and its corresponding tokenizer
-    model_ckpt = "distilbert-base-uncased"
-    tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
-
+def subword_tokenization_demo(tokenizer: AutoTokenizer):
     # example text
     text = "Tokenizing text is a core task of NLP."
 
@@ -41,3 +28,42 @@ if __name__ == "__main__":
     print(
         f"Input names expected by {model_ckpt}: {tokenizer.model_input_names}"
     )
+
+
+def tokenize_dataset(data: hfd.DatasetDict):
+    """`batched=True` will encode the data in batches. `batch_size=None` means
+    that the tokenization will be applied to the whole dataset as a
+    single batch. This ensures the `input_ids` and `attention_mask` have the
+    same shape globally.
+    """
+    data_encoded = data.map(tokenize, batched=True, batch_size=None)
+    return data_encoded
+
+
+def tokenize(batch):
+    return tokenizer(batch["text"], padding=True, truncation=True)
+
+
+if __name__ == "__main__":
+    # load dataset
+    emotions = hfd.load_dataset("emotion")
+
+    print(emotions)
+    train_ds = emotions["train"]
+    print(train_ds.column_names)
+    print(train_ds[15:17])
+
+    # load model and its corresponding tokenizer
+    model_ckpt = "distilbert-base-uncased"
+    tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+
+    # subword tokenization
+    subword_tokenization_demo(tokenizer)
+
+    # tokenize demo for a small batch from the dataset
+    batch_tokens = tokenize(train_ds[:2])
+    print(batch_tokens)
+
+    # tokenize the whole dataset
+    emotions_encoded = tokenize_dataset(emotions)
+    print(emotions_encoded["train"].column_names)
